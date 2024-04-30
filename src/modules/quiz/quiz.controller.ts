02cefa0a19a659a3603fb4cbe5982fd4cacbebd8
Felix Ajibola <oguntomilola.felix@adexfe.com>
2024-04-30 15:51:03 +0100

    Updated the getQuizById Type

--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { CreateQuizDto } from './dto/create-quiz.dto';
+import { Quiz } from './quiz.entity';
 
 @Controller('quiz')
 export class QuizController {
@@ -21,7 +22,7 @@ export class QuizController {
   }
 
   @Get('/:id')
-  async getQuizById(@Param('id', ParseIntPipe) id: number) {
+  async getQuizById(@Param('id', ParseIntPipe) id: number): Promise<Quiz> {
     return await this.quizService.getQuizById(id);
   }
 
